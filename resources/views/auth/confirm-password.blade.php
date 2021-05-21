f272586a28e538291c29a5073390b78de2cbac7d
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-05-21 14:39:43 +0200

    chore: Instalo Laravel Telescope

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -5,6 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
+            {{ __('Ésta es un área segura de la aplicación. Por favor introduzca su contraseña para continuar.') }}
             {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
         </div>
 
