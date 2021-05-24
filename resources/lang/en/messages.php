d89b3814ea8f418bcf14c241525aa874925d7752
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-05-24 20:45:25 +0200

    feat: Añado mensajes a la aplicación en español
    Mediante resources/lang/es

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Cancel' => 'Cancel',
+    'Password' => 'Password',
+    'LogIn' => 'LogIn',
+    'LogOut' => 'LogOut',
+    'Confirm' => 'Confirm',
+    'Dashboard' => 'Dashboard',
+    'Done' => 'Done.',
+    'Profile' => 'Profile',
+    'Saved' => 'Saved.',
+    'Save' => 'Save',
+    'manage_account' => 'Manage accout',
+];
