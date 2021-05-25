91465d2f3ff24a86fb5bf55fb6a59b8b5f71d0de
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-05-25 22:01:56 +0200

    feat: Validación de registro de email en backend
    En caso de que un correo inválido no se pare en frontend, el backend
    también valida (con la misma lista de dominios) y devuelve error.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Validator;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Modificado de https://stackoverflow.com/questions/31204093/laravel-registration-register-only-users-that-own-an-email-from-a-specific-doma
+        Validator::extend('allowed_domain', function($attribute, $value, $parameters, $validator) {
+            return in_array(explode('@', $value)[1], config('misc.valid_register_domains'));
+        }, __('messages.invalid_register_domain') );
     }
 }
