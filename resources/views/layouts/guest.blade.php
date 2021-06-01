b0e86f38ed29f6fcfb4ad1bc77039594774ba57e
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-01 18:48:46 +0200

    fix: Añado soporte de Blade-UI-Kit a la plantilla de invitados

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,10 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+        @livewireStyles
+
+        @bukStyles
+
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
@@ -20,5 +24,10 @@
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
+
+        @livewireScripts
+
+        @bukScripts
+
     </body>
 </html>
