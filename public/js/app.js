02487138b77359b41ff7cfe4f280963191be3f6c
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-08 12:07:48 +0200

    chore: Actualizo dependencias y algún metadato

--- public/js/app.js
+++ public/js/app.js
@@ -21402,7 +21402,7 @@ process.umask = function() { return 0; };
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunklaravel8_tests"] = self["webpackChunklaravel8_tests"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
