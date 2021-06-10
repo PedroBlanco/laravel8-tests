9b936eb549aedd52f19b154f1a4315cca28661de
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-10 15:22:47 +0200

    refactor: Cambio respuesta por algo más legible
    Aunque tengo que probar que funciona bien.

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -17,19 +17,23 @@ class CheckRole
     public function handle(Request $request, Closure $next, string $role)
     {
         if ( ($role == 'super') && (auth()->user()->role()->first()->nombre != 'SuperAdministrador') ) {
-            abort( 403 );
+            // abort( 403 );
+            abort ( Response::HTTP_FORBIDDEN );
         }
 
         if ( ($role == 'admin') && (auth()->user()->role()->first()->nombre != 'Administrador') ) {
-            abort( 403 );
+            // abort( 403 );
+            abort ( Response::HTTP_FORBIDDEN );
         }
 
         if ( ($role == 'editor') && (auth()->user()->role()->first()->nombre != 'Editor') ) {
-            abort( 403 );
+            // abort( 403 );
+            abort ( Response::HTTP_FORBIDDEN );
         }
 
         if ( ($role == 'user') && (auth()->user()->role()->first()->nombre != 'Usuario autenticado') ) {
-            abort( 403 );
+            // abort( 403 );
+            abort ( Response::HTTP_FORBIDDEN );
         }
 
         return $next($request);
